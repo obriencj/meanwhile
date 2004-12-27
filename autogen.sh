db1578449397c3860f2f266fdbfcc5614f4f7fae
Christopher O'Brien <obriencj@gmail.com>
2004-12-27 17:38:34 +0000

    fixing scripts

--- autogen.sh
+++ autogen.sh
@@ -2,34 +2,39 @@
 
 
 # just to help us differentiate messages
-function ECHO() {
-	echo "[AUTOGEN]" $*
+ECHO() {
+    echo "[AUTOGEN]" $*
 }
 
+
+quiet_test() {
+    sh -c "$*" >/dev/null 2>&1
+    return $?
+}
 
 
 # OSX has glibtoolize, everywhere else is just libtoolize
 if test -z "${LTIZE}" ; then
-	ECHO "Trying to find a libtoolize"
-
-	if sh -c "libtoolize --version" 2>&1 1> /dev/null ; then
-		LTIZE=libtoolize
-	elif sh -c "glibtoolize --version" 2>&1 1> /dev/null ; then
-		LTIZE=glibtoolize
-	fi
+    ECHO "Trying to find a libtoolize"
+    
+    if quiet_test "libtoolize --version" ; then
+	LTIZE=libtoolize
+    elif quiet_test "glibtoolize --version" ; then
+	LTIZE=glibtoolize
+    fi
 fi
 if test -z "${LTIZE}" ; then
-	ECHO "Couldn't figure out a libtoolize to use. Specify one with LTIZE"
+    ECHO "Couldn't figure out a libtoolize to use. Specify one with LTIZE"
 else
-	ECHO "Running $LTIZE"
-	$LTIZE || exit $?
+    ECHO "Running $LTIZE"
+    $LTIZE || exit $?
 fi
 
 
 
 # let's make sure we can find our aclocal macros
 if test -d /usr/local/share/aclocal ; then
-	ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
+    ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
 fi
 
 ECHO "Running aclocal $ACLOCAL_FLAGS"
@@ -44,7 +49,7 @@ aclocal $ACLOCAL_FLAGS || exit $?
 
 # put in license and stuff if necessary
 if test -z "$AUTOMAKE_FLAGS" ; then
-	AUTOMAKE_FLAGS="--add-missing --copy"
+    AUTOMAKE_FLAGS="--add-missing --copy"
 fi
 
 ECHO "Running automake $AUTOMAKE_FLAGS"
@@ -60,13 +65,14 @@ autoconf || exit $?
 ECHO "Running automake"
 automake || exit $?
 if test `uname` = "Darwin" ; then
-	automake Makefile 2> /dev/null
+    automake Makefile 2> /dev/null
 fi
 
 
 
 ECHO "Running ./configure $@"
 ./configure $@
+
 
 
 ECHO "Done"
