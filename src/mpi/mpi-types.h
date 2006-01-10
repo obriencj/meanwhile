9430813f7c40d5061e673dc3c4d4c525f3d3455f
Christopher O'Brien <obriencj@gmail.com>
2006-01-10 15:46:30 +0000

    fixing build problems on suse due to older glib

--- src/mpi/mpi-types.h
+++ src/mpi/mpi-types.h
@@ -8,9 +8,20 @@ typedef gsize              mw_mp_size;
 typedef gint               mw_mp_err;
 
 #define MP_DIGIT_BIT       16
+
+#ifdef G_MAXUINT16
 #define MP_DIGIT_MAX       G_MAXUINT16
+#else
+#define MP_DIGIT_MAX       ((guint16) 0xffff)
+#endif
+
 #define MP_WORD_BIT        32
+
+#ifdef G_MAXUINT32
 #define MP_WORD_MAX        G_MAXUINT32
+#else
+#define MP_WORD_MAX        ((guint32) 0xffffffff)
+#endif
 
 #define RADIX              (MP_DIGIT_MAX+1)
 
